0d22095dfddcd67b6004af7d2b2c32fb50a65aa7
Igor Karpov <ikarpov@cs.utexas.edu>
2011-09-22 11:46:59 -0500

    terminate on close, document

--- multiwx.py
+++ multiwx.py
@@ -1,8 +1,11 @@
+import wx
 from multiprocessing import Process, Queue
 
-import wx
-class MyFrame(wx.Frame):
-    """ We simply derive a new class of Frame. """
+__author__ = "Igor Karpov <ikarpov@cs.utexas.edu>"
+__date__ = "2011/09/22"
+
+class MultiFrame(wx.Frame):
+    """ A frame with a button """
     def __init__(self, parent, title, queue):
         wx.Frame.__init__(self, parent, title=title, size=(200,100))
         self.Bind(wx.EVT_CLOSE, self.OnClose)
@@ -12,21 +15,34 @@ class MyFrame(wx.Frame):
         self.Show(True)
 
     def OnButton(self,e):
+        """ when the button is pressed, send something to main process """
         self.queue.put('button!')
 
     def OnClose(self,e):
-        q.close()
+        """ when the window is closing, send None to main process """
+        print 'closing!'
+        self.queue.put(None)
         self.Destroy()
 
 def start_app(queue):
+    """ start a wxPython app using queue for multiprocess communication """
     app = wx.App(False)
-    frame = MyFrame(None, 'Menu', queue)
+    frame = MultiFrame(None, 'MultiWx', queue)
     app.MainLoop()
 
-if __name__ == '__main__':
+def main():
+    """ main function """
     q = Queue() # create a queue object to talk to the window
     p = Process(target=start_app, args=(q,)) # create the window process
     p.start() # start the window process
-    while q:
-        print q.get() # get stuff from the queue
-    p.join()
+    while True: # repeat until done
+        item = q.get() # get the next item from queue
+        if item is None:
+            print 'done!' # signal that we are done
+            break # break out of the loop
+        else:
+            print item # print the item from queue
+    p.join() # wait for the child process to finish
+
+if __name__ == '__main__':
+    main()
